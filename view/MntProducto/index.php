1ef23d1b29b8228176d9da1dfbd431d73033b228
JuanMa-19 <manuelqc1997@gmail.com>
2025-12-11 00:18:09 -0500

    Cambie el index de mntProducto, al logo le puse Juan

--- view/MntProducto/index.php
+++ view/MntProducto/index.php
@@ -44,7 +44,7 @@
 <body>
 
     <!-- ########## START: LEFT PANEL ########## -->
-    <div class="br-logo"><a href=""><span>[</span>bracket<span>]</span></a></div>
+    <div class="br-logo"><a href=""><span>[</span>Juan<span>]</span></a></div>
     <div class="br-sideleft overflow-y-auto">
         <label class="sidebar-label pd-x-15 mg-t-20">Navigation</label>
         <div class="br-sideleft-menu">
