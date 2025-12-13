d4a3cb44f5a40a0ac9b91eff3da6c44bee6688be
JuanMa-19 <manuelqc1997@gmail.com>
2025-12-13 13:36:27 -0500

    Se cambio la plantilla, se agrego la bd, y se act modelos

---
+++ config/conexion.php
@@ -0,0 +1,18 @@
+<?php
+class Conexion{
+    protected $dbh;
+    protected function Conexion(){
+        try {
+            $conectar = $this->dbh = new PDO("mysql:local=localhost;dbname=crud2","root","");
+            return $conectar;
+        } catch (Exception $e) {
+            print "Error en la BD: " . $e->getMessage() . "<br>";
+            die();
+        }
+    }
+
+    public function set_names(){
+        return $this->dbh->query("SET NAMES 'utf8'"); 
+    }
+}
+?>
